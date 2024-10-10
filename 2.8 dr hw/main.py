b59c9cb804834e0abb41147028d7df0468be1629
Insomnia01001 <167763964+Insomnia01001@users.noreply.github.com>
2024-10-10 19:50:11 +0300

    Add files via upload

---
+++ 2.8 dr hw/main.py
@@ -0,0 +1,19 @@
+# import math
+# mevalar = {'olma': 12000, 'banan':25000, 'gilos': 20000, 'shaftoli': 16000, 'uzum':23000}
+# result_sort = sorted(mevalar.values())
+# print(result_sort)
+# min=result_sort[0]
+# max=result_sort[-1]
+# print(min,max)
+
+# for i in mevalar:
+#     print(f"{i} - {mevalar[i]}")
+
+# ismlar = ['Akmal', 'Abror', 'Behruz', "Aziz"]
+# for i in range(len(ismlar)):
+#     print(f"{i} {ismlar[i]} ")
+
+students= {'Akmal': 11, 'Abror': 9, 'Behruz': 13, 'Husan': 12, 'Xurshid': 10}
+# result= sorted(students.values())
+result = sorted(students.values())
+print(result)
